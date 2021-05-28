94516a5f26486a47c3b2dd936434abaee443e43d
Alejandro <alejandropardo88@gmail.com>
2021-05-27 20:16:52 -0500

    Primer Commit

---
+++ parcial3web/src/Componentes/Content2.js
@@ -0,0 +1,17 @@
+import '../App.css';
+
+
+function App(){
+    return(
+        <div className = "content2">
+            <input placeholder="Shorten a link here...">
+            
+            </input>
+            <button className = "button2">
+            ShortenIt!
+            </button>
+        </div>
+    );
+}
+
+export default App;
---
+++ parcial3web/src/Componentes/Content3.js
@@ -0,0 +1,16 @@
+import '../App.css';
+
+function App(){
+    return(
+        <div className="content3">
+            <h1>
+                Advanced Statistics
+            </h1>
+            <p>
+                Track how your links are performing across the web with our advanced statistics dashboard.
+            </p>
+        </div>
+    );
+}
+
+export default App;
